e311893b525d76cf8e818598b89724ce9c983f50
Peleg <45825608+pkd2210@users.noreply.github.com>
2025-07-24 17:57:43 +0300

    Added the text of time

--- index.py
+++ index.py
@@ -1,14 +1,16 @@
 ### Imports ###
 from datetime import datetime
-import PIL  # Python Imaging Library is used to read and modify the images used
+from PIL import Image 
+from PIL import ImageFont
+from PIL import ImageDraw
 import os  # Used for interacting safely with the operating system to access my secret (environmental) variable
 import ctypes  # ctypes is used specifically to change the wallpaper
 import requests  # Used to get earth images for the background
+from datetime import datetime
 
 ### Constants ###
-imagePath = 'C:\\Users\\pkd22\\OneDrive\\Desktop\\Wallpaper\\wallpaper.png'  # Replace with the DIRECT file path to your wallpaper
+imagePath = 'PATHHHH'  # Replace with the DIRECT file path to your wallpaper
 SPI_SETDESKWALLPAPER = 20  # Tells Windows API to perform action 20: set desktop wallpaper
-APIKey = os.getenv("API-KEY")
 url = 'https://cdn.star.nesdis.noaa.gov/GOES19/ABI/FD/GEOCOLOR/10848x10848.jpg'
 
 def setWallpaper(imagePath):
@@ -21,6 +23,15 @@ def fetchEarthPicture(imagePath):
             file.write(response.content)
     else:
         print(f"Failed to download image. Status code: {response.status_code}")
-
 fetchEarthPicture(imagePath)
+img = Image.open(imagePath)
+font = ImageFont.truetype("font.ttf", 1000)
+currentDate = datetime.now().strftime("%d/%m - %H:%M")
+text = "EARTH AT: " + currentDate
+draw = ImageDraw.Draw(img)
+textArea = draw.textbbox((0, 0), text, font=font)
+textWidth = textArea[2] - textArea[0]
+draw.text(((img.width - textWidth) / 2, img.height/2), text, font=font, fill="white", stroke_fill="black", stroke_width=50)
+img.save(imagePath) 
+
 setWallpaper(imagePath)
